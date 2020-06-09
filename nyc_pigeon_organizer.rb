a604f5f9bb649b47908d78bde0dff3b1cb79c10d
Dmitry Sychev <axiumone@gmail.com>
2020-06-09 12:56:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  arr = []
+  data.each do |k1, v1|
+    v1.each do |k2,v2|
+    arr << v2
+    end
+  end
+  names_arr = arr.flatten.uniq
+
+  names_arr.each do |name|
+    pigeon_list[name] = {color: [], gender: [], lives: []}
+  end
+  pigeon_list
 end
